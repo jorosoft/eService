c74520d660f93994f2ec9df141f3e58d34b06200
jorosoft <jorosoftbg@gmail.com>
2018-01-20 21:59:51 +0200

    Update DB models

--- eService.Data.Models/Order.cs
+++ eService.Data.Models/Order.cs
@@ -1,15 +1,28 @@
-﻿using eService.Data.Models.Abstractions;
-using System;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using eService.Data.Models.Abstractions;
 
 namespace eService.Data.Models
 {
     public class Order : DBEntity
     {
+        [Required]
         public int Number { get; set; }
 
+        [Required]
         public DateTime Date { get; set; }
 
-        public int InvoiceNumber { get; set; }
+        public int WarrantyCardNumber { get; set; }
+
+        public DateTime WarrantyCardDate { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string Article { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string Defect { get; set; }
 
         public string Info { get; set; }
 
