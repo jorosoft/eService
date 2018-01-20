829ec12ef4658a79d3a440ee53f419310cb9ab04
jorosoft <jorosoftbg@gmail.com>
2018-01-20 12:39:27 +0200

    Created simple project structure

---
+++ eService.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eService.Web.MvcApplication" Language="C#" %>
---
+++ eService.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eService.Web.Startup))]
+namespace eService.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
