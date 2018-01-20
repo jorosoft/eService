142c8c731a174e08294139c89245d5a29a3892f6
jorosoft <jorosoftbg@gmail.com>
2018-01-20 13:35:53 +0200

    Added some libraries

--- eService.Web/App_Start/IdentityConfig.cs
+++ eService.Web/App_Start/IdentityConfig.cs
@@ -51,14 +51,14 @@ namespace eService.Web
             };
 
             // Configure validation logic for passwords
-            manager.PasswordValidator = new PasswordValidator
-            {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
-            };
+            //manager.PasswordValidator = new PasswordValidator
+            //{
+            //    RequiredLength = 6,
+            //    RequireNonLetterOrDigit = true,
+            //    RequireDigit = true,
+            //    RequireLowercase = true,
+            //    RequireUppercase = true,
+            //};
 
             // Configure user lockout defaults
             manager.UserLockoutEnabledByDefault = true;
--- eService.Web/Models/IdentityModels.cs
+++ eService.Web/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace eService.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("eServiceConnection", throwIfV1Schema: false)
         {
         }
 
